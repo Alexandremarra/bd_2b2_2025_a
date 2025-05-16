d74aa3bb077a5148e89a74d286f298d76fbac67a
victoricoma <victor.icoma@etec.sp.gov.br>
2025-05-16 18:10:27 -0300

    Aula0516 - Select Básico

---
+++ aula0516/AULA0516_SELECT_BASICO.sql
@@ -0,0 +1,8 @@
+/*Dados Empregados*/
+select * from empregado;
+/*Filtros de Dados*/
+select * from empregado where sexo='M';
+select * from empregado where sexo='F';
+
+select * from dependente;
+select * from departamento; 
